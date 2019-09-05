f72db7986752c71727b18b0265941c65320dbeb0
zhongjun2 <jun.zhongjun2@gmail.com>
2019-09-05 17:47:03 +0800

    Add config file

--- collector/collector.go
+++ collector/collector.go
@@ -1,3 +1,17 @@
+// Copyright 2019 HuaweiCloud.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package collector
 
 import (
@@ -65,7 +79,7 @@ func (exporter *BaseHuaweiCloudExporter) Describe(ch chan<- *prometheus.Desc) {
 
 
 func (exporter *BaseHuaweiCloudExporter) Collect(ch chan<- prometheus.Metric) {
-	periodm, _ := time.ParseDuration("-1m")
+	periodm, _ := time.ParseDuration("-4m")
 
 	now := time.Now()
 	from := strconv.FormatInt(int64(now.Add(periodm).UnixNano() / 1e6),10)
